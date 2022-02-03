24434c6bbced291c471afab934992cfbb4a5edc5
Lior Subotnick <theawesomepandapig@gmail.com>
2022-02-02 21:48:13 -0800

    Site Update

--- index.js
+++ index.js
@@ -16,7 +16,17 @@ function getFaqColor() {
     document.getElementById("faq-link").style.borderColor = color;
 }
 
+function getAboutColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.getElementById("about-link").style.borderColor = color;
+}
+
 function resetColor() {
     document.getElementById("contact-link").style.borderColor = "white";
     document.getElementById("faq-link").style.borderColor = "white";
+    document.getElementById("about-link").style.borderColor = "white";
 }
